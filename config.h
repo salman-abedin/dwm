c84704c9d6d7d8e3db6f5d85f006e318ae4cabab
salman-abedin <salman104alvi@gmail.com>
2020-08-24 16:13:30 +0600

    debut

--- config.h
+++ config.h
@@ -18,6 +18,10 @@ static const char *fonts[] = {
 
 static const char dmenufont[] = "monospace:size=15";
 
+//==============================================================================
+//                             Colors
+//==============================================================================
+
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
@@ -32,15 +36,10 @@ static const char sel_fg[] = "#ffffff";
 static const char sel_bg[] = "#000000";
 static const char sel_border[] = "#0000ff";
 
-static const char urg_fg[] = "#e5e9eb";
-static const char urg_bg[] = "#269886";
-static const char urg_border[] = "#269886";
-
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {norm_fg, norm_bg, norm_border},
     [SchemeSel] = {sel_fg, sel_bg, sel_border},
-    /* [SchemeUrg] = {urg_fg, urg_bg, urg_border}, */
 };
 
 static const char *tags[] = {
