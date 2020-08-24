da5e3507ac47ba90195bada39adaf4e21a7481c6
salman-abedin <salman104alvi@gmail.com>
2020-08-24 19:41:03 +0600

    emoji fix

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
 
 static const int vertpad = 10;  /* vertical padding of bar */
-static const int sidepad = 200; /* horizontal padding of bar */
+static const int sidepad = 250; /* horizontal padding of bar */
 
 static const char *fonts[] = {
     "monospace:size=15",
@@ -67,8 +67,8 @@ static const int resizehints =
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"| 🗃", tile}, /* first entry is default */
-    {"| 🎸", monocle},
+    {"| 🗃" |, tile}, /* first entry is default */
+    {"| 🎸 |", monocle},
     {"><>", NULL}, /* no layout function means floating behavior */
 };
 
