e14511458f01210d3690ca2562a666897e9310e3
salman-abedin <salman104alvi@gmail.com>
2020-08-24 14:05:42 +0600

    debut

--- config.h
+++ config.h
@@ -1,28 +1,49 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 0; /* border pixel of windows */
+static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+
+/* static const char *fonts[] = {"monospace:size=12"}; */
+
+static const char *fonts[] = {
+    "monospace:size=15",
+    "Noto Color Emoji:pixelsize=15:antialias=true:autohint=true",
+};
+
+static const char dmenufont[] = "monospace:size=15";
+
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
+
+static const char norm_fg[] = "#ffffff";
+static const char norm_bg[] = "#000000";
+static const char norm_border[] = "#ffffff";
+
+static const char sel_fg[] = "#ffffff";
+static const char sel_bg[] = "#555555";
+static const char sel_border[] = "#555555";
+
+static const char urg_fg[] = "#e5e9eb";
+static const char urg_bg[] = "#269886";
+static const char urg_border[] = "#269886";
+
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+    [SchemeNorm] = {norm_fg, norm_bg, norm_border},
+    [SchemeSel] = {sel_fg, sel_bg, sel_border},
+    /* [SchemeUrg] = {urg_fg, urg_bg, urg_border}, */
 };
 
-/* tagging */
 static const char *tags[] = {
-    "1",
-    "2",
-    "3",
+    "",
+    "🕸",
+    "😀",
 };
 
 static const Rule rules[] = {
@@ -32,7 +53,7 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor */
     {"Gimp", NULL, NULL, 0, 1, -1},
-    /* {"Firefox", NULL, NULL, 1 << 8, 0, -1}, */
+    {"firefox", NULL, NULL, 2, 1, -1},
 };
 
 /* layout(s) */
@@ -43,18 +64,14 @@ static const int resizehints =
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"[]=", tile}, /* first entry is default */
-    {"[M]", monocle},
+    {"| ", tile}, /* first entry is default */
+    {"| ", monocle},
     {"><>", NULL}, /* no layout function means floating behavior */
 };
 
-/* key definitions */
-#define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                         \
-   {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
-       {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
-       {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
-       {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+//==============================================================================
+//                             Commands
+//==============================================================================
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                         \
@@ -70,8 +87,27 @@ static const char *dmenucmd[] = {
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 
+//==============================================================================
+//                             Keyboard Bindings
+//==============================================================================
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY, TAG)                                         \
+   {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
+       {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
+       {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
+       {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+
+/* #include <X11/XF86keysym.h> */
+#include "patches/nextprevtag.c"
+#include "patches/shiftview.c"
 static Key keys[] = {
     /* modifier                     key        function        argument */
+
+    //--------------------------------------
+    //              WM stuff
+    //--------------------------------------
 
     {MODKEY, XK_l, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
@@ -79,37 +115,71 @@ static Key keys[] = {
     /* {MODKEY, XK_j, shiftview, {.i = -1}}, */
     /* {MODKEY, XK_semicolon, shiftview, {.i = +1}}, */
 
+    {MODKEY, XK_semicolon, view_adjacent, {.i = +1}},
+    {MODKEY, XK_j, view_adjacent, {.i = -1}},
+
     {MODKEY, XK_n, killclient, {0}},
     {MODKEY, XK_m, setlayout, {0}},
 
-    /* {MODKEY, XK_p, spawn, {.v = dmenucmd}}, */
-    /* {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}}, */
-    /* {MODKEY, XK_b, togglebar, {0}}, */
-    /* {MODKEY, XK_j, focusstack, {.i = +1}}, */
-    /* {MODKEY, XK_k, focusstack, {.i = -1}}, */
-    /* {MODKEY, XK_i, incnmaster, {.i = +1}}, */
-    /* {MODKEY, XK_d, incnmaster, {.i = -1}}, */
-    /* {MODKEY, XK_h, setmfact, {.f = -0.05}}, */
-    /* {MODKEY, XK_l, setmfact, {.f = +0.05}}, */
+    {MODKEY, XK_Tab, view, {0}},
+
+    /* Resize */
+    {MODKEY | Mod1Mask, XK_j, setmfact, {.f = -0.05}},
+    {MODKEY | Mod1Mask, XK_semicolon, setmfact, {.f = +0.05}},
+
+    /* {MODKEY | ShiftMask, XK_b, togglebar, {0}}, */
+
     /* {MODKEY, XK_Return, zoom, {0}}, */
-    /* {MODKEY, XK_Tab, view, {0}}, */
-    /* {MODKEY | ShiftMask, XK_c, killclient, {0}}, */
+
     /* {MODKEY, XK_t, setlayout, {.v = &layouts[0]}}, */
     /* {MODKEY, XK_f, setlayout, {.v = &layouts[1]}}, */
     /* {MODKEY, XK_m, setlayout, {.v = &layouts[2]}}, */
-    /* {MODKEY, XK_space, setlayout, {0}}, */
     /* {MODKEY | ShiftMask, XK_space, togglefloating, {0}}, */
-    /* {MODKEY, XK_0, view, {.ui = ~0}}, */
-    /* {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}}, */
-    /* {MODKEY, XK_comma, focusmon, {.i = -1}}, */
-    /* {MODKEY, XK_period, focusmon, {.i = +1}}, */
-    /* {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}}, */
-    /* {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}}, */
 
+    /* {MODKEY, XK_q, quit, {0}}, */
+
+    //--------------------------------------
+    //              Commands
+    //--------------------------------------
+
+    {MODKEY, XK_Return, spawn, SHCMD("launch -T")},
+
+    {MODKEY, XK_b, spawn, SHCMD("$BROWSER")},
+    {MODKEY, XK_e, spawn, SHCMD("insertemoji")},
+    {MODKEY, XK_o, spawn, SHCMD("bolt --tmux-search")},
+    {MODKEY, XK_p, spawn, SHCMD("mirror --git")},
+    {MODKEY, XK_r, spawn, SHCMD("dmenu_run")},
+    {MODKEY, XK_t, spawn, SHCMD("alacritty")},
+    {MODKEY, XK_w, spawn, SHCMD("setdisplay --bg shuffle")},
+
+    {0, XK_F1, spawn, SHCMD("setplayer --vol toggle")},
+    {0, XK_F2, spawn, SHCMD("setplayer --vol down")},
+    {0, XK_F3, spawn, SHCMD("setplayer --vol up")},
+    {0, XK_F11, spawn, SHCMD("backlight --down")},
+    {0, XK_F12, spawn, SHCMD("backlight --up")},
+
+    {0, XK_Print, spawn, SHCMD("screencast")},
+
+    {0, XK_KP_Home, spawn, SHCMD("connectbt")},
+    {0, XK_KP_End, spawn, SHCMD("pkill -u $USER")},
+    {0, XK_KP_Left, spawn, SHCMD("timer")},
+    {0, XK_KP_Delete, spawn, SHCMD("pkill -u $USER")},
+    {0, XK_KP_Insert, spawn, SHCMD("sleep 1 && xset dpms force off")},
+    {0, XK_KP_Add, spawn, SHCMD("systectl suspend")},
+
+    //--------------------------------------
+    //              Tags
+    //--------------------------------------
+
+    /* {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}}, // Sticky */
+    {MODKEY, XK_0, view, {.ui = ~0}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
-};
+            TAGKEYS(XK_9, 8)};
+
+//==============================================================================
+//                             Mouse Bindings
+//==============================================================================
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
