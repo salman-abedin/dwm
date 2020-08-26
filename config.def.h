88d3287fdc8f4cdf7be8458d0711a6e56f415485
salman-abedin <salman104alvi@gmail.com>
2020-08-26 20:03:15 +0600

    added bar height

--- config.def.h
+++ config.def.h
@@ -6,10 +6,11 @@ static const unsigned int gappx = 25;   /* gap pixel between windows */
 
 static const unsigned int snap = 32; /* snap pixel */
 
-static const int showbar = 1; /* 0 means no bar */
-static const int topbar = 1;  /* 0 means bottom bar */
-
-static const int vertpad = 10;  /* vertical padding of bar */
+static const int showbar = 1;  /* 0 means no bar */
+static const int topbar = 1;   /* 0 means bottom bar */
+static const int user_bh = 25; /* 0 means that dwm will calculate bar height, >=
+                                 1 means dwm will user_bh as bar height */
+static const int vertpad = 5;  /* vertical padding of bar */
 static const int sidepad = 390; /* horizontal padding of bar */
 
 static const char *fonts[] = {
