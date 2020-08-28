bab153f8d0cc9fb491826f3cee4c7922308b8dca
salman-abedin <salman104alvi@gmail.com>
2020-08-28 12:58:24 +0600

    added focusonactive patch

--- config.def.h
+++ config.def.h
@@ -62,9 +62,9 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    /* {"Gimp", NULL, NULL, 0, 1, -1}, */
+    {"st-256color", 0, 0, 1, 0, -1},
     {"firefox", 0, 0, 2, 0, -1},
-    {"mpv", 0, 0, -1, 1, -1},
+    {"mpv", 0, 0, 0, 1, -1},
 };
 
 /* layout(s) */
