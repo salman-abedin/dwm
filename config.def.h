43c7d189a864f5b284efa7a20a294999f22d132f
salman-abedin <salman104alvi@gmail.com>
2020-09-07 10:00:17 +0600

    added alttag patch

--- config.def.h
+++ config.def.h
@@ -18,7 +18,7 @@ static const int topbar = 1;   /* 0 means bottom bar */
 static const int user_bh = 25; /* 0 means that dwm will calculate bar height, >=
                                  1 means dwm will user_bh as bar height */
 static const int vertpad = 5;  /* vertical padding of bar */
-static const int sidepad = 390; /* horizontal padding of bar */
+static const int sidepad = 385; /* horizontal padding of bar */
 
 static const char *fonts[] = {
     "sans-serif:size=15",
